b9d27d3e4cea149a2ebe88bbdf4a1d3a44ca0f34
HOPE <boukhatemabdelilah420@gmail.com>
2024-04-17 22:12:27 +0100

    Hlogo-bugfix

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,9 @@ import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 import Projects from "@/components/Projects";
 import Technologies from "@/components/technologies";
-import { motion, useScroll } from "framer-motion";
-import { useRef } from "react";
 
 export default function Home() {
-  const ref = useRef<HTMLElement>(null)
-  const { scrollYProgress }=useScroll({
-    target: ref,
-    offset: ["0 1", "1.33 1"]
-  })
+
   return (
     
     <main className="h-full w-full  bg-[url('/LooperGroup2.png')] bg-no-repeat">
